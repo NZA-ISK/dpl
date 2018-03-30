afaa38073b1a550709466c28c6e32a60d21b75d2
gmarraffa <marraffa3ai@gmail.com>
2018-03-30 18:50:37 +0200

    Added new options and set the future work

--- lib/dpl/provider/google_drive.rb
+++ lib/dpl/provider/google_drive.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'google_drive'
 require 'zip'
 
 module DPL
@@ -7,6 +8,13 @@ Step per funzionare:
   1) Creare un service account
   2) Condivedere una cartella del proprio drive con il service account
 
+ENV VARS
+GDRIVE_SERVICE_ACCOUNT --> stringa json criptata credenziali service account
+--> (encrypted via "travis encrypt" command line tool)
+
+SCRIPT VARS
+shared --> directory condivisa con il service account
+project --> root directory da caricare in drive
 
 =end
   class Provider
@@ -18,9 +26,6 @@ Step per funzionare:
         end
         GoogleDrive::Session.from_service_account_key(file_name)
       end
-      def json_credentials
-        session.upload_from_file("credentials.json", "bpippo.json", convert: false)
-      end
 
       def check_auth
         raise Error, "Please add GDRIVE_SERVICE_ACCOUNT in Travis settings" unless context.env['GDRIVE_SERVICE_ACCOUNT']
@@ -30,11 +35,25 @@ Step per funzionare:
         File.expand_path( (context.env['TRAVIS_BUILD_DIR'] || '.' ) + "/" + (options[:project] || '') )
       end
 
+      def drive_root
+        shared_dir = session.file_by_name(shared)
+      end
+
+      def shared
+        options[:shared]
+      end
+
+      def zipped
+        options[:zip] || false
+      end
+
       def check_app
         raise Error, "Please set a 'project' folder path under 'deploy' in .travis.yml" unless File.directory?(project)
+        raise Error, "Please set a 'shared' folder path under 'deploy' in .travis.yml" if drive_root.nil?
       end
 
       def push_app
+        # TODO: implementare algoritmo ricorsione
         # session.file_by_title("nome_cartella").upload_from_file("wewe")
         # TODO: deploy the code
       end
